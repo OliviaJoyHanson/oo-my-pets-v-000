dfc74d52e957d64c1ddbce6749610c8c89ca1931
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-09-17 16:42:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,40 +29,38 @@ class Owner
   end
 
   def buy_fish(name)
-    self.pets[:fishes] << Fish.new(name)
+    @pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    self.pets[:cats] << Cat.new(name)
+    @pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    self.pets[:dogs] << Dog.new(name)
+    @pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    self.pets[:dogs].collect do |dog|
+    @pets[:dogs].collect do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    self.pets[:cats].collect do |cat|
+    @pets[:cats].collect do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    self.pets[:fishes].collect do |fish|
+    @pets[:fishes].collect do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-    self.pets.each do |species|
-      species[1].collect do |pet|
-        pet.mood = "nervous"
-      end
+    @pets.collect do |species, pets|
+      pets.each{|pet| pet.mood = "nervous"}.clear
       species[1].clear
     end
   end
